563b46949de8accceaf1ca7c4a63a3378fb3a1db
ivan <ivan_petrov0341@abv.bg>
2024-08-24 14:17:44 +0300

    Removing the namespaces

--- WebApp12/City.cs
+++ WebApp12/City.cs
@@ -1,20 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace WebApp12;
 
-namespace WebApp12
+public class City
 {
-    public class City
-    {
-        public string Name { get; set; }
-        public string Address { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
 
-        public City(string name,string adress)
-        {
-            Name = adress;
-            Address = name;
-        }
+    public City(string name,string adress)
+    {
+        Name = adress;
+        Address = name;
     }
 }
