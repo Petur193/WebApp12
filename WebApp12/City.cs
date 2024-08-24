edae59338c3a5e27d42a2bff85ef01c31ed6051e
ivan <ivan_petrov0341@abv.bg>
2024-08-24 14:16:30 +0300

    refactoring add city

---
+++ WebApp12/City.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp12
+{
+    public class City
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public City(string name,string adress)
+        {
+            Name = adress;
+            Address = name;
+        }
+    }
+}
